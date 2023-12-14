3daf38afcbdf05df06b56891faf50a0918a1e5a2
Pedro Matos <109925892+pedrodabahia@users.noreply.github.com>
2023-12-13 22:00:28 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(' i').click(function(){    
+        var el =  $(this).parent().find('ul')
+
+        if(el.is(':visible') == false){
+            el.fadeIn();
+        }else{
+            el.fadeOut();
+        }
+
+    })
+
+})
